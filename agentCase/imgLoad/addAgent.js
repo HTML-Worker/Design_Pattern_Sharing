84fe4de80b908af1d9211a04e41b288d4e0db291
鲍龙 <baolong@gridsum.com>
2018-01-31 02:06:15 +0800

    完成设计模式分享ppt初稿

--- agentCase/imgLoad/addAgent.js
+++ agentCase/imgLoad/addAgent.js
@@ -32,3 +32,21 @@ console.log(proxyImage);
 // 注：本体和代理都需要显式的实现同一方法，提高程序健壮性。
 // 可以通过鸭子类型来检测，但是大多数时候是不做检测的，依靠程序员的自觉性。
 // 代理和本体可替换使用。
+// 虚拟代理合并HTTP请求（案例北知文书送达预览）
+
+/*
+不使用代理实现图片预加载：
+let myImage = (() => {
+  let imgNode = document.createElement('img');
+  document.body.appendChild(imgNode);
+  img.onload = () => {
+    myImage.setSrc('./imgs/loadImg.jpg');
+  }
+  return {
+    setSrc: (src) => {
+      imgNode.src = src;
+    }
+  }
+})();
+myImage.setSrc('http://img17.3lian.com/d/file/201702/15/2da7ad2722e0fec8a7eec97216d1cdb5.jpg');
+*/
